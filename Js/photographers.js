0c083881e3566f971053feb8862b44ff60b8395a
Jonathan Chosson <catstrike@gmail.com>
2021-07-28 15:38:36 +0200

    [JS] Chargement des medias :rocket: Par artistes/types médias

--- Js/photographers.js
+++ Js/photographers.js
@@ -1,6 +1,6 @@
 //Elements du DOM
 let artiste = document.getElementsByClassName('photographers__header');
-let oeuvre = document.getElementsByClassName('photographers__article__div');
+let oeuvre = document.getElementsByClassName('photographers__article');
 //Fonction
 function afficheArtiste(){
     if (window.location.search.split("=").length > 1){
@@ -38,7 +38,59 @@ function afficheArtiste(){
 }
 //Affiche les médias de l'artiste
 function mediaArtiste() {
-
+    if (window.location.search.split("=").length > 1){
+        console.log('medias');
+        let artisteId = window.location.search.split("=")[1];
+        let mediaArtiste = [];
+        console.log(artisteId);
+        fetch('./FishEyeData.json').then((response) => response.json())
+        .then((data) =>{
+            console.log(data.media);
+            data.media.forEach(media => {
+                if(media.photographerId == artisteId){
+                    mediaArtiste.push(media)
+                }
+            });
+            console.log(mediaArtiste);
+            console.log(oeuvre[0].children[0]);
+            oeuvre[0].removeChild(oeuvre[0].children[0]);
+            for(media of mediaArtiste){
+                console.log(media);
+                let div = document.createElement('div');
+                div.classList.add('photographers__article__div');
+                let figure = document.createElement('figure');
+                figure.classList.add('photographers__article__div__figure');
+                let img = document.createElement('img');
+                img.classList.add('photographers__article__div__figure__img');
+                let video = document.createElement('video');
+                video.classList.add('photographers__article__div__figure');
+                video.setAttribute('controls', "");
+                if(media.image !=undefined){
+                    img.setAttribute('src',`./public/images/${media.photographerId}/${media.image}`);
+                    figure.append(img);
+                    div.append(figure);
+                }else if(media.video !=undefined){
+                    let sourceVideo = document.createElement('source');
+                    sourceVideo.setAttribute('src',`./public/images/${media.photographerId}/${media.video}`);
+                    sourceVideo.setAttribute('type', "video/mp4");
+                    video.append(sourceVideo);
+                    div.append(video);
+                }
+                let pTitre = document.createElement('p');
+                pTitre.classList.add('photographers__article__div__p--titre');
+                pTitre.innerHTML =`${media.title}`;
+                pLikes = document.createElement('p');
+                pLikes.classList.add('photographers__article__div__p--likes');
+                pLikes.innerHTML = `${media.likes} <i class="fas fa-heart"></i>`;
+                div.append(pTitre);
+                div.append(pLikes);
+                oeuvre[0].append(div);
+            }
+        }).catch(erreur => console.log(erreur));
+    }else{
+        document.location.href="./index.html"; 
+    }
 }
 //app
 afficheArtiste();
+mediaArtiste();
